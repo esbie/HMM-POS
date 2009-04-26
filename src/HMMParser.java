d9f991f3f25dabe02ffec282dd7e627c5d0191ff
Sarah Brown <esbie.is@gmail.com>
2009-04-26 16:18:44 -0400

    adding parser. seems to be working

---
+++ src/HMMParser.java
@@ -0,0 +1,64 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class HMMParser {
+    
+    public static void main(String[] args){
+        HMMParser p = new HMMParser("data/test.pos");
+        p.parse();        
+    }
+    
+    Scanner scanner;
+    
+    HashMap<String, Integer> tagCounts = new HashMap<String, Integer>();
+    HashMap<String, HashMap<String, Integer>> wordCounts = new HashMap<String, HashMap<String,Integer>>();
+    HashMap<String, HashMap<String, Integer>> tagBigramCounts = new HashMap<String, HashMap<String,Integer>>();
+    
+    
+    public HMMParser(String filename){
+        File file = new File(filename);
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void parse(){
+        String prevTag = scanner.next();
+        scanner.next();
+       
+        while(scanner.hasNext()){
+            String currentTag = scanner.next();
+            String currentWord = scanner.next();
+            
+            addOne(tagCounts, currentTag);
+            addOne(wordCounts, currentTag, currentWord);
+            addOne(tagBigramCounts, prevTag, currentTag);
+            
+            prevTag = currentTag;
+        }
+    }
+    
+    private void addOne(HashMap<String, Integer> map, String key1){
+        if(map.containsKey(key1)){
+            map.put(key1, map.get(key1)+1);
+        } else{
+            map.put(key1, 1);
+        }       
+    }   
+    
+    private void addOne(HashMap<String, HashMap<String, Integer>> map, String key1, String key2){
+        if(map.containsKey(key1)){
+            addOne(map.get(key1),key2);
+        } else {
+            HashMap<String, Integer> subMap = new HashMap<String, Integer>();
+            subMap.put(key2, 1);
+            map.put(key1, subMap);
+        }
+    }    
+}
